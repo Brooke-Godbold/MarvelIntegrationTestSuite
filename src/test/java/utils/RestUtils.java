d08ea05dbf8b8a9a41066b2510bf171fa485c165
Brooke <brookegodbold13@googlemail.com>
2020-05-18 15:01:40 +0100

    Updating POM, removing redundant code

--- src/test/java/utils/RestUtils.java
+++ src/test/java/utils/RestUtils.java
@@ -1,17 +1,10 @@
 package utils;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.hateoas.hal.Jackson2HalModule;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.Collections;
 
 public class RestUtils {
 
@@ -37,15 +30,4 @@ public class RestUtils {
         return restTemplate.getForEntity(uri, String.class);
     }
 
-    private static RestTemplate restTemplate() {
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        mapper.registerModule(new Jackson2HalModule());
-
-        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
-        converter.setSupportedMediaTypes(MediaType.parseMediaTypes("application/hal+json"));
-        converter.setObjectMapper(mapper);
-
-        return new RestTemplate(Collections.<HttpMessageConverter<?>> singletonList(converter));
-    }
 }
